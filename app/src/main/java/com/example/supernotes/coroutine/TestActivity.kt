b01b41f6e1ebbceca553ae5d09a4076e2ebae373
mrphonglinh <mrphonglinh2605@gmail.com>
2022-01-03 23:18:22 +0700

    format doc

--- app/src/main/java/com/example/supernotes/coroutine/TestActivity.kt
+++ app/src/main/java/com/example/supernotes/coroutine/TestActivity.kt
@@ -2,10 +2,20 @@ package com.example.supernotes.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.supernotes.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+        sayHello()
+        println("World!")
+    }
+
+    fun sayHello() = runBlocking {
+        println("Hello")
+        delay(5000)
     }
 }

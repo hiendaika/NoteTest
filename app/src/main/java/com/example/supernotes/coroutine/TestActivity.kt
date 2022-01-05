a50eedd4ca35461b76e3deb5d788fa1b09f59060
hien_dz <mrphonglinh2605@gmail.com>
2022-01-05 17:31:19 +0700

    add topic 5-6-7

--- app/src/main/java/com/example/supernotes/coroutine/TestActivity.kt
+++ app/src/main/java/com/example/supernotes/coroutine/TestActivity.kt
@@ -2,20 +2,292 @@ package com.example.supernotes.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.supernotes.R
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import java.io.IOException
+import kotlin.system.measureTimeMillis
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        sayHello()
-        println("World!")
+//        sayHello()
+//        println("World!")
+
+//        Module 5 Async Await
+//        _5_assync_1()
+//        sum2Number()
+//        sum2NumberWithLazy()
+
+//        Module 6 Coroutine Scope
+//        testScope()
+//        cancelParent()
+
+//        MODULE 7: Exception
+//            testException()
+//        testSupervisorJob()
+//        testSupervisorScope()
+
+//        MODULE 8: FLOW
+        calculateTime()
     }
 
     fun sayHello() = runBlocking {
         println("Hello")
         delay(5000)
     }
+
+    //Async giup tra ve ket qua
+    fun _5_assync_1() = runBlocking {
+        val int: Deferred<Int> = async { printInt() }
+        val string: Deferred<String> = async { return@async "HienDZ" }
+        val unit: Deferred<Unit> = async { }
+    }
+
+    fun printInt(): Int {
+        return 10
+    }
+
+    //VD tinh tong 2 so nhan duoc tu ket qua tra ve cua 2 function
+    suspend fun printOne(): Int {
+        delay(1000)
+        return 10
+    }
+
+    suspend fun printTwo(): Int {
+        delay(1000)
+        return 12
+    }
+
+    fun sum2Number() = runBlocking {
+        val time = measureTimeMillis {
+            val one = async { printOne() }
+            val two = async { printTwo() }
+            Log.d("Module5", "Sum: ${one.await() + two.await()}")
+        }
+        Log.d("Module5", "Time: ${time}")
+    }
+
+    fun sum2NumberWithLazy() = runBlocking {
+        val time = measureTimeMillis {
+            val one = async(start = CoroutineStart.LAZY) { printOne() }
+            val two = async(start = CoroutineStart.LAZY) { printTwo() }
+//            one.start() // start thang one
+//            two.start() // start thang two
+            Log.d("Module5", "Sum: ${one.await() + two.await()}")
+        }
+        Log.d("Module5", "Time: ${time}")
+    }
+
+
+    //Module 6 Coroutine Scope
+    //Custom Scope
+    @Suppress("FunctionName")
+    fun MainScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
+    //    Coroutine nested coroutine
+    fun testScope() = runBlocking { // scope 1
+        launch {       // coroutine 1
+            delay(200L)
+            Log.d("Module6", "Task from runBlocking")   // line code 1
+        }
+
+        coroutineScope { // coroutine 2   // scope 2
+            launch {   // coroutine 3
+                delay(500L)
+                Log.d("Module6", "testScope:Task from nested launch") // line code 2
+            }
+
+            delay(100L)
+            Log.d("Module6", "Task from coroutine scope") // line code 3
+        }
+
+        Log.d("Module6", "Coroutine scope is over") // line code 4
+    }
+
+    //Cancel parent coroutine
+    fun cancelParent() = runBlocking<Unit> {
+        val request = launch {
+            launch {
+                delay(100)
+                Log.d("Module6", "job2: I am a child of the request coroutine")   // line code 1
+                delay(1000)
+                Log.d(
+                    "Module6",
+                    "job2: I will not execute this line if my parent request is cancelled"
+                ) // line code 2
+            }
+        }
+        delay(500)
+        request.cancel() // cancel processing of the request
+        delay(1000)
+        Log.d("Module6", "main: Who has survived request cancellation?") // line code 3
+    }
+
+    //    GlobalScope
+    fun testGlobalScope() = runBlocking<Unit> {
+        val request = launch {
+            // it spawns two other jobs, one with GlobalScope
+            GlobalScope.launch {
+                Log.d("Module6", "job1: GlobalScope and execute independently!")
+                delay(1000)
+                Log.d(
+                    "Module6",
+                    "job1: I am not affected by cancellation"
+                )  // line code 1 này vẫn được in ra mặc dù bị delay 1000ms
+            }
+            // and the other inherits the parent context
+            launch {
+                delay(100)
+                Log.d("Module6", "job2: I am a child of the request coroutine")
+                delay(1000)
+                Log.d(
+                    "Module6",
+                    "job2: I will not execute this line if my parent request is cancelled"
+                )
+            }
+        }
+        delay(500)
+        request.cancel() // cancel processing of the request
+        delay(1000) // delay a second to see what happens
+        Log.d("Module6", "main: Who has survived request cancellation?")
+    }
+
+
+    //    MODULE 7: Exception
+    // Test exception
+    fun testException() = runBlocking {
+        GlobalScope.launch {
+            try {
+                Log.d("Module7", "Throwing exception from launch")
+                throw IndexOutOfBoundsException()
+                Log.d("Module7", "Unreached")
+            } catch (e: IndexOutOfBoundsException) {
+                Log.d("Module7", "Caught IndexOutOfBoundsException")
+            }
+        }
+
+        val deferred = GlobalScope.async {
+            Log.d("Module7", "Throwing exception from async")
+            throw ArithmeticException()
+            Log.d("Module7", "Unreached")
+        }
+        try {
+            deferred.await()
+            Log.d("Module7", "Unreached")
+        } catch (e: ArithmeticException) {
+            Log.d("Module7", "Caught ArithmeticException")
+        }
+    }
+
+    //Tong hop nhieu exception
+    fun totalException() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            Log.d(
+                "Module7",
+                "Caught $exception with suppressed ${exception.suppressed.contentToString()}"
+            )
+        }
+        val job = GlobalScope.launch(handler) {
+            launch {
+                try {
+                    delay(Long.MAX_VALUE) // delay vô hạn
+                } finally {
+                    throw ArithmeticException()
+                }
+            }
+            launch {
+                try {
+                    delay(Long.MAX_VALUE) // delay vô hạn
+                } finally {
+                    throw IndexOutOfBoundsException()
+                }
+            }
+            launch {
+                delay(100)
+                throw IOException()
+            }
+            delay(Long.MAX_VALUE)
+        }
+        job.join()
+    }
+
+    //    Supervisor Job
+    fun testSupervisorJob() = runBlocking {
+        val supervisor = SupervisorJob()
+        with(CoroutineScope(coroutineContext + supervisor)) {
+            // launch the first child -- its exception is ignored for this example (don<t do this in practice!)
+            val firstChild = launch(CoroutineExceptionHandler { _, _ -> }) {
+                Log.d("Module7", "First child is failing")
+                throw AssertionError("First child is cancelled")
+            }
+            // launch the second child
+            val secondChild = launch {
+                firstChild.join()
+                // Cancellation of the first child is not propagated to the second child
+                Log.d(
+                    "Module7",
+                    "First child is cancelled: ${firstChild.isCancelled}, but second one is still active"
+                )
+                try {
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    // But cancellation of the supervisor is propagated
+                    Log.d("Module7", "Second child is cancelled because supervisor is cancelled")
+                }
+            }
+            // wait until the first child fails & completes
+            firstChild.join()
+            Log.d("Module7", "Cancelling supervisor")
+            supervisor.cancel()
+            secondChild.join()
+        }
+    }
+
+    //    Supervisor Scope
+    fun testSupervisorScope() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            Log.d("Module7", "Caught $exception")
+        }
+        supervisorScope {
+            val first = launch(handler) {
+                Log.d("Module7", "Child throws an exception")
+                throw AssertionError()
+            }
+            val second = launch {
+                delay(100)
+                Log.d("Module7", "Scope is completing")
+            }
+        }
+        Log.d("Module7", "Scope is completed")
+    }
+
+
+    //    MODULE 8: FLOW
+    /*
+    * So sanh Collection, Sequences, Flow bang cach xay dung function foo
+    * Do thoi gian thuc thi ham foo.
+    * */
+//    Su dung collection
+    suspend fun fooCollection(): List<Int> {
+        val list = mutableListOf<Int>()
+        for (i in 1..3) {
+            delay(1000)
+            list.add(i)
+        }
+
+        return list
+    }
+    //Su dung
+
+    //    tinh toan thoi gian thuc hien
+    fun calculateTime() = runBlocking {
+        val time = measureTimeMillis {
+            fooCollection().forEach { value -> Log.d("Module8", "Time: $value") }
+        }
+        println(time)
+    }
+
+
 }
